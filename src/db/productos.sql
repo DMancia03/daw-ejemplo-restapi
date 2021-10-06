7352dbef483d7aad6471c8248320057f61f4983b
Diego Mancía <62128556+DiegoSan03@users.noreply.github.com>
2021-10-05 22:48:53 -0600

    Bases de datos

--- src/db/productos.sql
+++ src/db/productos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 06-10-2021 a las 04:21:35
+-- Tiempo de generación: 06-10-2021 a las 04:44:42
 -- Versión del servidor: 8.0.18
 -- Versión de PHP: 7.4.0
 
@@ -35,6 +35,7 @@ CREATE TABLE IF NOT EXISTS `productos` (
   `descripcion` mediumtext COLLATE utf8_spanish2_ci NOT NULL,
   `tipoProducto` varchar(45) COLLATE utf8_spanish2_ci NOT NULL,
   `imgUrl` mediumtext COLLATE utf8_spanish2_ci NOT NULL,
+  `precio` decimal(10,0) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
@@ -42,11 +43,11 @@ CREATE TABLE IF NOT EXISTS `productos` (
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id`, `nombre`, `descripcion`, `tipoProducto`, `imgUrl`) VALUES
-(1, 'Cartucho TLOZ BOTW', 'Juego con partida todo desbloqueado.', 'Juegos usados', 'https://www.zelda.com/breath-of-the-wild/assets/icons/BOTW-Share_icon.jpg'),
-(3, 'STARTUP GAMER 3000', 'Asombroso equipo gamer que incluye una pc gamer y una silla, todo lleno de RGB como debe ser.', 'Equipo informático', 'https://i.blogs.es/ed843e/superpc-ap/450_1000.jpeg'),
-(8, 'PS3 casi nuevo', 'Una consola PlayStation 3 casi nueva, solo la compre y no la ocupe.  No incluye juegos :/', 'Equipo informático', 'https://m.media-amazon.com/images/I/41J29bRxQVL._AC_SY1000_.jpg'),
-(9, 'WII U NUEVO', 'Es de los pocos wii u en perfecto estado que vas a encontrar, no incluye juegos pero es una consola nuevita.', 'Equipo informático', 'https://cdn.pocket-lint.com/r/s/1200x/assets/images/73040-games-review-nintendo-wii-u-review-image1-n7bs4eY69z.jpg');
+INSERT INTO `productos` (`id`, `nombre`, `descripcion`, `tipoProducto`, `imgUrl`, `precio`) VALUES
+(1, 'Cartucho TLOZ BOTW', 'Juego con partida todo desbloqueado.', 'Juegos usados', 'https://www.zelda.com/breath-of-the-wild/assets/icons/BOTW-Share_icon.jpg', '0'),
+(3, 'STARTUP GAMER 3000', 'Asombroso equipo gamer que incluye una pc gamer y una silla, todo lleno de RGB como debe ser.', 'Equipo informático', 'https://i.blogs.es/ed843e/superpc-ap/450_1000.jpeg', '0'),
+(8, 'PS3 casi nuevo', 'Una consola PlayStation 3 casi nueva, solo la compre y no la ocupe.  No incluye juegos :/', 'Equipo informático', 'https://m.media-amazon.com/images/I/41J29bRxQVL._AC_SY1000_.jpg', '0'),
+(9, 'WII U NUEVO', 'Es de los pocos wii u en perfecto estado que vas a encontrar, no incluye juegos pero es una consola nuevita.', 'Equipo informático', 'https://cdn.pocket-lint.com/r/s/1200x/assets/images/73040-games-review-nintendo-wii-u-review-image1-n7bs4eY69z.jpg', '0');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
